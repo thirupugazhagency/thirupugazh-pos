5a6c16feeda86bfc2402c7b03afd26a7f7725f9f
Venkatesh <venkateshemirates1@gmail.com>
2026-02-09 03:36:20 +0530

    Temp remove user.status to allow app boot

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False)
-    status = db.Column(db.String(20), default="ACTIVE")
 
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
